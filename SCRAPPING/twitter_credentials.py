38a569ee72482fde45bd06cd5c9d5beb5788cd69
samwata <samsonamwata16@gmail.com>
2021-03-10 22:04:01 +0300

    data and notebook updates

---
+++ SCRAPPING/twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "Your App Access Token"
+ACCESS_TOKEN_SECRET = "Your App Token Secret Key"
+CONSUMER_KEY = "Your App Consumer Key"
+CONSUMER_SECRET = "Your App Consumer Secret"
